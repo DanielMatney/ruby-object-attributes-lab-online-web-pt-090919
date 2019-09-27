749835473e5f7246b14b22bd99a66934df79a918
Daniel Matney <danieljmatney@gmail.com>
2019-09-27 22:39:56 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+  
+  def name=(their_name)
+    @name = their_name
+  end
+  
+  def name
+    @name
+  end
+  
+end
+
+class Person
+  
+  def job=(their_job)
+    @job = their_job
+  end
+  
+  def job
+    @job
+  end
+end
